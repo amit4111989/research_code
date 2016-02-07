61b711a8115ef08cfc8145290d6414659773b462
Amit Juneja <amitj@email.arizona.edu>
2016-02-07 15:28:21 -0700

    added general anomaly file

--- logistic_sgd.py
+++ logistic_sgd.py
@@ -1,37 +1,3 @@
-"""
-This tutorial introduces logistic regression using Theano and stochastic
-gradient descent.
-
-Logistic regression is a probabilistic, linear classifier. It is parametrized
-by a weight matrix :math:`W` and a bias vector :math:`b`. Classification is
-done by projecting data points onto a set of hyperplanes, the distance to
-which is used to determine a class membership probability.
-
-Mathematically, this can be written as:
-
-.. math::
-  P(Y=i|x, W,b) &= softmax_i(W x + b) \\
-                &= \frac {e^{W_i x + b_i}} {\sum_j e^{W_j x + b_j}}
-
-
-The output of the model or prediction is then done by taking the argmax of
-the vector whose i'th element is P(Y=i|x).
-
-.. math::
-
-  y_{pred} = argmax_i P(Y=i|x,W,b)
-
-
-This tutorial presents a stochastic gradient descent optimization method
-suitable for large datasets.
-
-
-References:
-
-    - textbooks: "Pattern Recognition and Machine Learning" -
-                 Christopher M. Bishop, section 4.3.2
-
-"""
 __docformat__ = 'restructedtext en'
 
 import cPickle
@@ -169,7 +135,7 @@ class LogisticRegression(object):
             raise NotImplementedError()
 
 
-def load_data(dataset):
+def load_data():
     ''' Loads the dataset
 
     :type dataset: string
@@ -180,39 +146,32 @@ def load_data(dataset):
     # LOAD DATA #
     #############
 
-    # Download the MNIST dataset if it is not present
-    data_dir, data_file = os.path.split(dataset)
-    if data_dir == "" and not os.path.isfile(dataset):
-        # Check if dataset is in the data directory.
-        new_path = os.path.join(
-            os.path.split(__file__)[0],
-            "..",
-            "data",
-            dataset
-        )
-        if os.path.isfile(new_path) or data_file == 'mnist.pkl.gz':
-            dataset = new_path
+    f = open('general_data_set_1.csv','r')
+    payload = f.read()
+    payload = payload.split('\n')
+    #train_set_x = []
+    train_set_x = []
+    train_y = []
+    for i in xrange(len(payload)):
+        payload[i] = payload[i].split(',')
+        if len(payload[i])>1:
+            payload[i] = [float(val) for val in payload[i]]
+            train_set_x.append(payload[i][:-5])
+            train_y.append(int(payload[i][-1]))
+        else:
+            del payload[i]
+    train_set = (np.array(train_set_x),train_y)
+  
+    
+    #train_set_x = numpy.array(train_set_x,dtype='f')
+    #train_set_x = numpy.asarray(train_set_x,dtype='f')
 
-    if (not os.path.isfile(dataset)) and data_file == 'mnist.pkl.gz':
-        import urllib
-        origin = (
-            'http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz'
-        )
-        print 'Downloading data from %s' % origin
-        urllib.urlretrieve(origin, dataset)
-
-    print '... loading data'
-
-    # Load the dataset
-    f = gzip.open(dataset, 'rb')
-    train_set, valid_set, test_set = cPickle.load(f)
-    f.close()
-    # train_set, valid_set, test_set format: tuple(input, target)
-    # input is a numpy.ndarray of 2 dimensions (a matrix)
-    # where each row corresponds to an example. target is a
-    # numpy.ndarray of 1 dimension (vector) that has the same length as
-    # the number of rows in the input. It should give the target
-    # to the example with the same index in the input.
+    #train_set, valid_set, test_set format: tuple(input, target)
+    #input is an numpy.ndarray of 2 dimensions (a matrix)
+    #witch row's correspond to an example. target is a
+    #numpy.ndarray of 1 dimensions (vector)) that have the same length as
+    #the number of rows in the input. It should give the target
+    #target to the example with the same index in the input.
 
     def shared_dataset(data_xy, borrow=True):
         """ Function that loads the dataset into shared variables
@@ -224,12 +183,23 @@ def load_data(dataset):
         variable) would lead to a large decrease in performance.
         """
         data_x, data_y = data_xy
-        shared_x = theano.shared(numpy.asarray(data_x,
+        shared_x = theano.shared(np.asarray(data_x,
                                                dtype=theano.config.floatX),
                                  borrow=borrow)
-        shared_y = theano.shared(numpy.asarray(data_y,
+        shared_y = theano.shared(np.asarray(data_y,
                                                dtype=theano.config.floatX),
                                  borrow=borrow)
+
+        # one-hot encoded labels as {-1, 1}
+        data_y = np.asarray(data_y, dtype='int32')
+        n_classes = len(np.unique(data_y))  # dangerous?
+        y1 = -1 * np.ones((data_y.shape[0], n_classes))
+        y1[np.arange(data_y.shape[0]), data_y] = 1
+
+        shared_y1 = theano.shared(np.asarray(y1,
+                                               dtype=theano.config.floatX),
+                                 borrow=borrow)
+
         # When storing data on the GPU it has to be stored as floats
         # therefore we will store the labels as ``floatX`` as well
         # (``shared_y`` does exactly that). But during our computations
@@ -237,14 +207,13 @@ def load_data(dataset):
         # floats it doesn't make sense) therefore instead of returning
         # ``shared_y`` we will have to cast it to int. This little hack
         # lets ous get around this issue
-        return shared_x, T.cast(shared_y, 'int32')
+        return shared_x, shared_y, T.cast(shared_y1,  'int32')
 
-    test_set_x, test_set_y = shared_dataset(test_set)
-    valid_set_x, valid_set_y = shared_dataset(valid_set)
-    train_set_x, train_set_y = shared_dataset(train_set)
+    #test_set_x, test_set_y, test_set_y1 = shared_dataset(test_set)
+    #valid_set_x, valid_set_y, valid_set_y1 = shared_dataset(valid_set)
+    train_set_x, train_set_y, train_set_y1 = shared_dataset(train_set)
 
-    rval = [(train_set_x, train_set_y), (valid_set_x, valid_set_y),
-            (test_set_x, test_set_y)]
+    rval = [(train_set_x, train_set_y, train_set_y1) ]
     return rval
 
 
