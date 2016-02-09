8dfab21ce95c3548ed5fc6a84d43fa0f88f8377c
Ubuntu <ubuntu@ip-172-31-46-189.us-west-2.compute.internal>
2016-02-09 04:07:34 +0000

    more figures

--- figure_maker.py
+++ figure_maker.py
@@ -8,8 +8,8 @@
 import matplotlib.pyplot as plt 
 import numpy as np 
 
-#file1 = open('data_extraction/classifier_data/105_V.csv','r')
-file1 = open('data_extraction/classifier_data_nobase/105_V.csv','r')
+file1 = open('data_extraction/classifier_data/105_V.csv','r')
+#file1 = open('data_extraction/classifier_data_nobase/105_V.csv','r')
 
 file1 = file1.read().split('\n')
 
@@ -26,4 +26,4 @@ for i in file1:
 plt.plot(x[0])
 plt.plot(x[1])
 
-plt.savefig('figures/fig1.png')
+plt.savefig('figures/fig2_base.png')
