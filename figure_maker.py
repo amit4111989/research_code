0f42777b6f1ece42a8c5b66900b534c36822085d
Amit Juneja <amitj@email.arizona.edu>
2016-02-12 02:49:25 -0700

    changed figure maker file

--- figure_maker.py
+++ figure_maker.py
@@ -8,6 +8,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+
+
 file1 = open('data_extraction/classifier_data/105_V.csv','r')
 #file1 = open('current_benchmarking/train_set.csv','r')
 #file1 = open('data_extraction/classifier_data_nobase/105_V.csv','r')
@@ -21,13 +23,31 @@ label_name = ['N','V','S','F','Q']
 colors = [(0.0, 0.0, 0.0),(0.0, 0.0, 1.0),(0.75, 0, 0.75),(1.0, 0.0, 0.0),(0.75, 0.75, 0)]
 
 x = []
-for i in file1:
-  i = i.split(',')
-  if len(i)>1:
-    j = [float(val) for val in i[:-5]]
-    x.append(j)
-  else:
-    del i
+
+for subdir, dirs, files in os.walk('data_extraction/classifier_data'):
+	for file in files:
+		with open(file,'r') as f:
+			pay = f.read().split('\n')
+			beats_arr = []
+			plt.figure(0)
+			for lines in pay:
+				line = lines.split(',')
+				if len(line)>1:
+					beats = [float(val) for val in line[:-5]]
+					plt.plot(beats,color=colors[1])
+					beats_arr.append(beats)
+				else:
+					del line
+			plt.savefig('figures/all_V/'+file[:-4]+'.png')
+
+# for i in file1:
+#   i = i.split(',')
+#   if len(i)>1:
+#     j = [float(val) for val in i[:-5]]
+#     x.append(j)
+#   else:
+#     del i
+
 
 # for i in file1:
 #   i = i.split(',')
@@ -39,14 +59,14 @@ for i in file1:
 #     del i
 
 # for i in xrange(5):
-#   for plts in labels[i]:
-#     plt.figure(i)
-#     plt.plot(plts,label=label_name[i],color=colors[i])
-#     plt.savefig('figures/'+label_name[i]+'_arrhythmia.png')
+# for plts in labels[i]:
+#    plt.figure(i)
+#    plt.plot(plts,label=label_name[i],color=colors[i])
+#    plt.savefig('figures/'+label_name[i]+'_arrhythmia.png')
 
-plt.plot(x[0],color=colors[0])
-plt.plot(x[1],color=colors[0])
-plt.plot(x[2],color=colors[1])
-plt.plot(x[3],color=colors[1])
+#plt.plot(x[0],color=colors[0])
+#plt.plot(x[1],color=colors[0])
+#plt.plot(x[2],color=colors[1])
+#plt.plot(x[3],color=colors[1])
 
-plt.savefig('figures/105_V_comparison.png')
+#plt.savefig('figures/105_V_comparison.png')
