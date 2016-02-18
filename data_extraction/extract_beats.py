843f195a0b32e3010ab6c1e701d2a8e4966a9895
Ubuntu <ubuntu@ip-172-31-39-176.us-west-2.compute.internal>
2016-02-18 05:00:59 +0000

    50 hidden 15 epochs

--- data_extraction/extract_beats.py
+++ data_extraction/extract_beats.py
@@ -54,7 +54,7 @@ def clean_signal(
 
 def shift_offset(ecg):
    """   Add an offset so that the signal stays within positive values """
-   ecg = [i+0.5 for i in ecg]
+   ecg = [i+0.3 for i in ecg]
    return ecg
 
 def delete_empty_vals(ecg):
@@ -80,7 +80,7 @@ def extract_signal(filename):
       i = i.split(',')
       if len(i)>1:
          temp = float(i[1])
-         temp = (temp)/255.875
+         temp = (temp)/2047
          output.append(temp)
    return output
 
