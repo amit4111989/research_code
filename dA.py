dffd101c6b68c274f3ac72f10f0b2500ab03b08b
Ubuntu <ubuntu@ip-172-31-39-176.us-west-2.compute.internal>
2016-02-20 07:52:59 +0000

    figures corrected

--- dA.py
+++ dA.py
@@ -425,8 +425,9 @@ def test_dA(learning_rate=0.3, training_epochs=50,hidden_units=50,
 	cost = test_da_real(idx)
         if not test_set_y[idx] ==0 and anom[int(test_set_y[idx])]:
         	plt.figure(idx)
-        	plt.plot(cost[0],'r')
-                plt.plot(cost[1],'b')
+		print cost
+        	plt.plot(cost[0][0][1],'r')
+                plt.plot(cost[1][0][1],'b')
                 plt.savefig('fig_%d_recon.png'%(test_set_y[idx]))
                 anom[int(test_set_y[idx])] = False
 	#if cost>0.2 and not test_set_y[idx]==0:
