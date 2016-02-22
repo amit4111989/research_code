d6d0a006122850603fea531cdbdce151fecb2e94
Ubuntu <ubuntu@ip-172-31-39-176.us-west-2.compute.internal>
2016-02-22 14:02:43 +0000

    yes

--- data_extraction/make_rqa_plots.py
+++ data_extraction/make_rqa_plots.py
@@ -7,7 +7,7 @@ file.close()
 
 
 
-labels = ['-%ds'%(i) for i in xrange(300,0,-10) ]
+labels = ['-%ds'%(i) for i in xrange(300,0,-5) ]
 
 rr = [i[0] for i in pay]
 det = [i[1] for i in pay]
@@ -21,7 +21,7 @@ rt = [i[9]/100.00 for i in pay]
 
 plt.figure(0)
 plt.xticks([i for i in xrange(51)][1:],labels,rotation='vertical')
-plt.margins(0.05)
+plt.margins(0.005)
 plt.plot(rr,label="RR")
 plt.plot(det,label="DET")
 plt.plot(avgdl,label="AVGDL")
@@ -31,6 +31,6 @@ plt.plot(ventr,label="VENTR")
 plt.plot(lam,label="LAM")
 plt.plot(mvert,label="MVERT")
 plt.plot(rt,label="RT")
-plt.legend(bbox_to_anchor=(-1,0,1.,1),loc=2,mode='expand', borderaxespad=0.)
+plt.legend(bbox_to_anchor=(0,0,1.,1),loc=4, borderaxespad=0.)
 plt.tight_layout()
 plt.savefig('rqa_vt.png')
