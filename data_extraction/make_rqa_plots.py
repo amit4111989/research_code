b3dbc6875814fae8251b574d0b140bff8fd4f167
root <root@ip-172-31-39-176.us-west-2.compute.internal>
2016-02-22 13:22:59 +0000

    goddamn rqa plot

--- data_extraction/make_rqa_plots.py
+++ data_extraction/make_rqa_plots.py
@@ -31,6 +31,6 @@ plt.plot(ventr,label="VENTR")
 plt.plot(lam,label="LAM")
 plt.plot(mvert,label="MVERT")
 plt.plot(rt,label="RT")
-plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.legend(loc=2, borderaxespad=0.)
 
 plt.savefig('rqa_vt.png')
